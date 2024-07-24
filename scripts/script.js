d05bda320ed4c23983c1d5ca0212320748cd1b4c
RangelMRK <marcosvrangel@hotmail.com>
2024-07-23 22:06:20 -0300

    Update screen 1200px

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ let divOriginal = `
 <p class="p2">Digite um texto que você deseja criptografar ou descriptografar</p>
 </div>
 </div>
-<div id="copy" class="copy__button" onclick="copyText()"><button class="copy__button__style">Copiar</button></div>`;
+<div id="copy" class="copy__button" onclick="copyText()"><button id="copy__button__style">Copiar</button></div>`;
 
 let cssOriginal = `  display: flex;
     flex-direction: column;
@@ -22,9 +22,31 @@ let cssOriginal = `  display: flex;
     height: 59rem;
 
 
-    background-color: white;`
+    background-color: white;`;
 
-let div2 = `
+    let divResize = `<div id="result">
+            <div id="paragrafos">
+            <p class="p1">Nenhuma mensagem encontrada</p>
+            <p class="p2">Digite um texto que você deseja criptografar ou descriptografar</p>
+            </div>
+            </div>
+            <div id="copy" class="copy__button" onclick="copyText()"><button id="copy__button__style">Copiar</button></div>`;
+    let cssResize = `margin-left: 2.5rem;
+
+    
+        padding: 2rem;
+        
+        width: 43rem;
+        border-radius: 2rem;
+        box-shadow: 0px 24px 32px -8px #00000014;
+    
+        height: 21.4rem;
+    
+    
+        background-color: white;`;
+
+
+let resuOriginal = `
         display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -41,11 +63,62 @@ let div2 = `
     background-color: white;
     `;
 
+let resuResize = `
+    display: flex;
+    justify-content: space-between;
+
+
+    box-sizing: border-box;
+    padding: 2rem;
+
+    margin-left: 2.5rem;
+    
+    width: 43rem;
+    border-radius: 2rem;
+    box-shadow: 0px 24px 32px -8px #00000014;
+    
+    height: 21.4rem;
+    
+    
+    background-color: white;
+    `;
+
+let copyOriginal = `
+    height: 4.20rem;
+    border-radius: 1.5rem;
+    width: 20.5rem;
+    background-color: white;
+    color:#0A3871;
+    border: 1px solid #0A3871;
+    `;
+
+let copyResize = `
+    height: 4.15rem;
+    border-radius: 1.5rem;
+    width: 39rem;
+    background-color: white;
+    color:#0A3871;
+    border: 1px solid #0A3871;`;
+
 function showCopy(){
         document.getElementById("copy").style.display='block';
 }
 function transformDiv(){
-    document.getElementById("text_show").style.cssText = div2;
+    var screenSize = window.matchMedia("(max-width: 1200px");
+
+    if(screenSize.matches)
+        {document.getElementById("text_show").style.cssText = resuResize}
+    else{
+    document.getElementById("text_show").style.cssText = resuOriginal};
+}
+
+function transformCopy(){
+    var screenSize = window.matchMedia("(max-width: 1200px");
+
+    if(screenSize.matches)
+        {document.getElementById("copy__button__style").style.cssText = copyResize}
+    else{
+        document.getElementById("copy__button__style").style.cssText = copyOriginal};
 }
 
 function hidePlaceholder(){
@@ -88,6 +161,7 @@ unlockButton();
 function encriptar() {
     showCopy();
     transformDiv();
+    transformCopy();
     var x = document.getElementById("input_area_text").value;
     var y = {
         e:"enter",
@@ -101,7 +175,9 @@ function encriptar() {
     z = x.replace(/e|i|a|o|u/gi, function(encript){
     return y[encript]});
 
-    document.getElementById("result").innerHTML = z;   
+    document.getElementById("result").innerHTML = z;
+    
+    cleanTextArea();
     
 }
 
@@ -109,6 +185,7 @@ function encriptar() {
 function desencriptar() {
         showCopy();
         transformDiv();
+        transformCopy();
     var x = document.getElementById("input_area_text").value;
     var y = {
         enter:'e',
@@ -121,11 +198,20 @@ function desencriptar() {
     z = x.replace(/enter|imes|ai|ober|ufat/gi, function(desencript){
     return y[desencript]});
     document.getElementById("result").innerHTML = z;
+    cleanTextArea();
 }
 
 function revertDiv(){
+    
+    var screenSize = window.matchMedia("(max-width: 1200px")
+
+    if(screenSize.matches)
+    {document.getElementById('text_show').innerHTML = divResize;
+        document.getElementById("text_show").style.cssText = cssResize;
+    }else{
     document.getElementById('text_show').innerHTML = divOriginal;
     document.getElementById("text_show").style.cssText = cssOriginal;
+    }
 }
 
 function copyText(){
@@ -133,5 +219,4 @@ function copyText(){
     navigator.clipboard.writeText(inlineTxt.innerHTML);
 
     revertDiv();
-    cleanTextArea();
 }
