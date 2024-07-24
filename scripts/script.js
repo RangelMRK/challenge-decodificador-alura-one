c0a2809733f816eefff0fbc53ab0d4dc0b31ffae
RangelMRK <marcosvrangel@hotmail.com>
2024-07-24 19:58:42 -0300

    Responsividade Arrumada, Limpada no Código.

--- scripts/script.js
+++ scripts/script.js
@@ -1,124 +1,16 @@
-let divOriginal = `
-<div id="result">
-<img src="assets/img/ilustration.svg" alt="Menina com uma Lupa olhando um cristal">
-<div id="paragrafos">
-<p class="p1">Nenhuma mensagem encontrada</p>
-<p class="p2">Digite um texto que você deseja criptografar ou descriptografar</p>
-</div>
-</div>
-<div id="copy" class="copy__button" onclick="copyText()"><button id="copy__button__style">Copiar</button></div>`;
-
-let cssOriginal = `  display: flex;
-    flex-direction: column;
-    justify-content: center;
-    box-sizing: border-box;
-
-    padding: 2rem;
-    
-    width: 25rem;
-    border-radius: 2rem;
-    box-shadow: 0px 24px 32px -8px #00000014;
-
-    height: 59rem;
-
-
-    background-color: white;`;
-
-    let divResize = `<div id="result">
-            <div id="paragrafos">
-            <p class="p1">Nenhuma mensagem encontrada</p>
-            <p class="p2">Digite um texto que você deseja criptografar ou descriptografar</p>
-            </div>
-            </div>
-            <div id="copy" class="copy__button" onclick="copyText()"><button id="copy__button__style">Copiar</button></div>`;
-    let cssResize = `margin-left: 2.5rem;
-
-    
-        padding: 2rem;
-        
-        width: 43rem;
-        border-radius: 2rem;
-        box-shadow: 0px 24px 32px -8px #00000014;
-    
-        height: 21.4rem;
-    
-    
-        background-color: white;`;
-
-
-let resuOriginal = `
-        display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    box-sizing: border-box;
-    padding: 2rem;
-    
-    width: 25rem;
-    border-radius: 2rem;
-    box-shadow: 0px 24px 32px -8px #00000014;
-
-    height: 59rem;
-
-
-    background-color: white;
-    `;
-
-let resuResize = `
-    display: flex;
-    justify-content: space-between;
-
-
-    box-sizing: border-box;
-    padding: 2rem;
-
-    margin-left: 2.5rem;
-    
-    width: 43rem;
-    border-radius: 2rem;
-    box-shadow: 0px 24px 32px -8px #00000014;
-    
-    height: 21.4rem;
-    
-    
-    background-color: white;
-    `;
-
-let copyOriginal = `
-    height: 4.20rem;
-    border-radius: 1.5rem;
-    width: 20.5rem;
-    background-color: white;
-    color:#0A3871;
-    border: 1px solid #0A3871;
-    `;
-
-let copyResize = `
-    height: 4.15rem;
-    border-radius: 1.5rem;
-    width: 39rem;
-    background-color: white;
-    color:#0A3871;
-    border: 1px solid #0A3871;`;
-
 function showCopy(){
-        document.getElementById("copy").style.display='block';
+        document.getElementById("result").style.display = 'flex';
 }
-function transformDiv(){
-    var screenSize = window.matchMedia("(max-width: 1200px");
-
-    if(screenSize.matches)
-        {document.getElementById("text_show").style.cssText = resuResize}
-    else{
-    document.getElementById("text_show").style.cssText = resuOriginal};
+function hideCopy(){
+    document.getElementById("result").style.display = 'none';
 }
 
-function transformCopy(){
-    var screenSize = window.matchMedia("(max-width: 1200px");
+function hideToogle(){
+    document.getElementById("toogleabe").style.display = 'none';
+}
 
-    if(screenSize.matches)
-        {document.getElementById("copy__button__style").style.cssText = copyResize}
-    else{
-        document.getElementById("copy__button__style").style.cssText = copyOriginal};
+function showToogle(){
+    document.getElementById("toogleabe").style.display = 'block';
 }
 
 function hidePlaceholder(){
@@ -133,10 +25,10 @@ function showPlaceholder() {
 }
 
 function lockButton() {
-    let botoes = document.getElementById("testes");
-    let botoess = botoes.querySelectorAll(".button__style")
+    let botoes = document.getElementById("buttons");
+    let selectBotoes = botoes.querySelectorAll(".button__style")
     if(document.getElementById("input_area_text").value==="") {
-        for(let i=0; i < botoess.length; i++)
+        for(let i=0; i < selectBotoes.length; i++)
         botoess[i].disabled = true}
         
 }
@@ -145,9 +37,9 @@ function unlockButton(){
     let areaInput = document.getElementById("input_area_text");
     areaInput.addEventListener("input", unlock);
 
-    function unlock(){let botoes = document.getElementById("testes");
-        let botoess = botoes.querySelectorAll(".button__style")
-            for(let i=0; i < botoess.length; i++)
+    function unlock(){let botoes = document.getElementById("buttons");
+        let selectBotoes = botoes.querySelectorAll(".button__style")
+            for(let i=0; i < selectBotoes.length; i++)
             botoess[i].disabled = false}
 }
 
@@ -159,9 +51,8 @@ lockButton();
 unlockButton();
 
 function encriptar() {
+    hideToogle();
     showCopy();
-    transformDiv();
-    transformCopy();
     var x = document.getElementById("input_area_text").value;
     var y = {
         e:"enter",
@@ -175,7 +66,7 @@ function encriptar() {
     z = x.replace(/e|i|a|o|u/gi, function(encript){
     return y[encript]});
 
-    document.getElementById("result").innerHTML = z;
+    document.getElementById("result_cripto").innerHTML = z;
     
     cleanTextArea();
     
@@ -183,9 +74,8 @@ function encriptar() {
 
 
 function desencriptar() {
+        hideToogle();
         showCopy();
-        transformDiv();
-        transformCopy();
     var x = document.getElementById("input_area_text").value;
     var y = {
         enter:'e',
@@ -197,26 +87,16 @@ function desencriptar() {
 
     z = x.replace(/enter|imes|ai|ober|ufat/gi, function(desencript){
     return y[desencript]});
-    document.getElementById("result").innerHTML = z;
+    document.getElementById("result_cripto").innerHTML = z;
     cleanTextArea();
 }
 
-function revertDiv(){
-    
-    var screenSize = window.matchMedia("(max-width: 1200px")
-
-    if(screenSize.matches)
-    {document.getElementById('text_show').innerHTML = divResize;
-        document.getElementById("text_show").style.cssText = cssResize;
-    }else{
-    document.getElementById('text_show').innerHTML = divOriginal;
-    document.getElementById("text_show").style.cssText = cssOriginal;
-    }
-}
 
 function copyText(){
-    let inlineTxt = document.getElementById("result");
+    let inlineTxt = document.getElementById("result_cripto");
     navigator.clipboard.writeText(inlineTxt.innerHTML);
 
-    revertDiv();
+    hideCopy();
+    showToogle();
+
 }
